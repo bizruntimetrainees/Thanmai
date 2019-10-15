2a4075df00ddb9fd771145764b13027f4362096a
Thanmai <thanmai.c@bizruntime.net>
2019-10-15 19:02:45 +0530

    Events

---
+++ 15-10-2019/Events/SubscribeEvent.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    // Define a class to hold event info
+    public class CustomEventArgs : EventArgs
+    {
+        private string message;
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+        public CustomEventArgs(string s)
+        {
+            message = s;
+        }
+      
+    }
+
+    //Declaring an event
+    class Publisher
+    { 
+        public event EventHandler<CustomEventArgs> RaiseCustomEvent;
+        public void DoSomething()
+        {
+            OnRaiseCustomEvent(new CustomEventArgs("Did something"));
+        }   
+       protected virtual void OnRaiseCustomEvent(CustomEventArgs e)
+        {
+            EventHandler<CustomEventArgs> handler = RaiseCustomEvent;
+            if (handler != null)
+            {
+                e.Message += $" at {DateTime.Now}";
+                handler(this, e);
+            }
+        }
+    }
+
+    //Class that subscribes to an event
+    class Subscriber
+    {
+        private string id;
+        public Subscriber(string ID, Publisher pub)
+        {
+            id = ID;
+            // Subscribe to the event 
+            pub.RaiseCustomEvent += HandleCustomEvent;
+        }
+
+        // Define what actions to take when the event is raised.
+        void HandleCustomEvent(object sender, CustomEventArgs e)
+        {
+            Console.WriteLine(id + " received this message: {0}", e.Message);
+        }
+    }
+    class SubscribeEvent
+        {
+            static void Main(string[] args)
+            {
+            Publisher pub = new Publisher();
+            Subscriber sub1 = new Subscriber("sub1", pub);
+            Subscriber sub2 = new Subscriber("sub2", pub);
+
+            pub.DoSomething();
+
+            Console.WriteLine("Press Enter to close this window.");
+            Console.ReadLine();
+        }
+        }
+    }
+
---
+++ 15-10-2019/InterfaceEvent/Interface_Evnent.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceEvent
+{
+    public interface IDrawingObject
+    {
+        // Raise this event before drawing the object.
+        event EventHandler OnDraw;
+    }
+    public interface IShape
+    {
+        // Raise this event after drawing the shape.
+        event EventHandler OnDraw;
+    }
+    public class Shape : IDrawingObject, IShape
+    {
+        // Create an event for each interface event
+        event EventHandler PreDrawEvent;
+        event EventHandler PostDrawEvent;
+
+        object objectLock = new Object();
+
+        // Explicit interface implementation required.
+        // Associate IDrawingObject's event with
+        // PreDrawEvent
+        #region IDrawingObjectOnDraw
+        event EventHandler IDrawingObject.OnDraw
+        {
+            add
+            {
+                lock (objectLock)
+                {
+                    PreDrawEvent += value;
+                }
+            }
+            remove
+            {
+                lock (objectLock)
+                {
+                    PreDrawEvent -= value;
+                }
+            }
+        }
+        #endregion
+      
+        event EventHandler IShape.OnDraw
+        {
+            add
+            {
+                lock (objectLock)
+                {
+                    PostDrawEvent += value;
+                }
+            }
+            remove
+            {
+                lock (objectLock)
+                {
+                    PostDrawEvent -= value;
+                }
+            }
+
+
+        }
+        public void Draw()
+        {
+            // Raise IDrawingObject's event before the object is drawn.
+            PreDrawEvent?.Invoke(this, EventArgs.Empty);
+
+            Console.WriteLine("Drawing a shape.");
+
+            // Raise IShape's event after the object is drawn.
+            PostDrawEvent?.Invoke(this, EventArgs.Empty);
+        }
+    }
+    public class Subscriber1
+    {
+        // References the shape object as an IDrawingObject
+        public Subscriber1(Shape shape)
+        {
+            IDrawingObject d = (IDrawingObject)shape;
+            d.OnDraw += d_OnDraw;
+        }
+
+        void d_OnDraw(object sender, EventArgs e)
+        {
+            Console.WriteLine("Sub1 receives the IDrawingObject event.");
+        }
+    }
+    // References the shape object as an IShape
+    public class Subscriber2
+    {
+        public Subscriber2(Shape shape)
+        {
+            IShape d = (IShape)shape;
+            d.OnDraw += d_OnDraw;
+        }
+
+        void d_OnDraw(object sender, EventArgs e)
+        {
+            Console.WriteLine("Sub2 receives the IShape event.");
+        }
+    }
+    class Interface_Evnent
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Shape();
+            Subscriber1 sub = new Subscriber1(shape);
+            Subscriber2 sub2 = new Subscriber2(shape);
+            shape.Draw();
+
+           Console.WriteLine("Press any key to exit.");
+           Console.ReadKey();
+        }
+    }
+}
