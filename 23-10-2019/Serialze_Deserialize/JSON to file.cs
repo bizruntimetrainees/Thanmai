4d54008db6cca5d39062e4d76fc153fc63954188
Thanmai <thanmai.c@bizruntime.net>
2019-10-23 18:51:13 +0530

    Serialize&Deserialize

---
+++ 23-10-2019/Serialze_Deserialize/JSON to file.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace Newtonsoft_JSON.NET
+{
+    class JSON_to_file
+    {
+        public string moviename { get; set; }
+        public int year{ get; set; }
+
+        public static void GetDetails()
+        {
+            JSON_to_file js = new JSON_to_file();
+            js.moviename = "NEW YORK";
+            js.year = 1998;
+
+            string op = JsonConvert.SerializeObject(js, Formatting.Indented);
+            Console.WriteLine("serialization Procees of another class start here ..........");
+            File.WriteAllText(@"D:\Newtonsoft JSON.NET\text.json", op);
+
+            using (StreamWriter file = File.CreateText(@"D:\Newtonsoft JSON.NET\text.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, op);
+            }
+
+
+
+            JSON_to_file js1 = JsonConvert.DeserializeObject<JSON_to_file>(File.ReadAllText(@"D:\Newtonsoft JSON.NET\text.json"));
+
+            Console.WriteLine("Deserialization Procees of another class start here ..........");
+
+            using (StreamReader file = File.OpenText(@"D:\Newtonsoft JSON.NET\text.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+
+                JSON_to_file js2 = (JSON_to_file)serializer.Deserialize(file, typeof(JSON_to_file));
+            }
+        }
+
+
+    }
+}
---
+++ 23-10-2019/Serialze_Deserialize/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace Newtonsoft_JSON.NET
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public double Price { get; set; }
+        public String[] Sizes{ get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product productobj = new Product();
+
+            productobj.Name = "Apple";
+            productobj.ExpiryDate = new DateTime(2008, 12, 28);
+            productobj.Price = 3.99;
+            productobj.Sizes = new string[] { "Small", "Medium", "Large" };
+
+            string output = JsonConvert.SerializeObject(productobj, Formatting.Indented);
+
+            Console.WriteLine("Serialization Procees start here ..........");
+            Console.WriteLine(output);
+            //Console.ReadKey();
+            Console.WriteLine(); 
+
+            Product deserializedProduct = JsonConvert.DeserializeObject<Product>(output);
+            Console.WriteLine("Deserialization Procees start here ..........");
+            Console.WriteLine(deserializedProduct.Name);
+            Console.WriteLine(deserializedProduct.ExpiryDate);
+            Console.WriteLine(deserializedProduct.Price);
+            Console.WriteLine(JsonConvert.SerializeObject(deserializedProduct.Sizes));
+        
+            Console.WriteLine();
+
+
+            //JSON TO FILE
+            JSON_to_file.GetDetails();
+
+            Console.ReadKey();
+
+        
+
+
+
+        }
+    }
+}
