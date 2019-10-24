ca180bab551776c6a8777e1bebef240ce47983ea
Thanmai <thanmai.c@bizruntime.net>
2019-10-24 22:07:47 +0530

    Callback

---
+++ 24-10-2019/Task/task.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace Task
+{
+    class Details
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+            public void GetDetails()
+            {
+                Console.Write("Enter Id:");
+                Id = Console.ReadLine();
+
+                Console.Write("Enter a name:");
+                Name = Console.ReadLine();
+
+            }
+             public void printInfo()
+              {
+                    Console.WriteLine("Record List: ");
+                    Console.WriteLine("\tId       : " + Convert.ToInt32(Id));
+                    Console.WriteLine("\tName     : " + Convert.ToString(Name));
+             }
+    }
+    class task
+    {
+       
+        static void Main(string[] args)
+        {
+            int size;
+            Console.WriteLine("Enter size:");
+            var tempData = Console.ReadLine();
+            size = Int32.Parse(tempData);
+            Console.WriteLine(size);
+           
+           
+            Details[] d = new Details[size];
+           
+            for (int i = 0; i < size; i++)
+            {
+                d[i]= new Details();
+            }
+         
+            for (int i=0;i<size;i++)
+            {
+              d[i].GetDetails();
+              d[i].printInfo();
+              Console.WriteLine();
+            }
+
+            string output = JsonConvert.SerializeObject(d, Formatting.Indented);
+            Console.WriteLine("Serialization Procees start here ..........");
+            Console.WriteLine(output);
+            Console.WriteLine();      
+
+            var deserializedoutput = JsonConvert.DeserializeObject<Details[]>(output);
+            Console.WriteLine("Deserialization Procees start here ..........");
+            foreach( var obj in deserializedoutput)
+            {
+                Console.WriteLine(obj.Name);
+                Console.WriteLine(obj.Id);
+            } 
+
+
+
+            Console.ReadKey();
+        }
+       
+    }
+}
---
+++ 24-10-2019/callback/call_back.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Callback
+{
+    public class SerializationEventTestObject
+    {
+       
+        public int Member1 { get; set; }    
+        public string Member2 { get; set; }
+
+        [JsonIgnore]
+        public string Member3 { get; set; }
+        public string Member4 { get; set; }
+
+        public SerializationEventTestObject()
+        {
+            Member1 = 11;
+            Member2 = "Welcome to Callback!";
+            Member3 = "This is a nonserialized value";
+            Member4 = null;
+        }
+
+        [OnSerializing]
+       public void OnSerializingMethod(StreamingContext context)
+        {
+            Member2 = "This value went into the data file during serialization.";
+        }
+
+        [OnSerialized]
+        public void OnSerializedMethod(StreamingContext context)
+        {
+            Member2 = "This value was reset after serialization.";
+        }
+
+        [OnDeserializing]
+        public void OnDeserializingMethod(StreamingContext context)
+        {
+            Member3 = "This value was set during deserialization";
+        }
+
+        [OnDeserialized]
+        public void OnDeserializedMethod(StreamingContext context)
+        {
+            Member4 = "This value was set after deserialization.";
+        }
+    }
+    class call_back
+    {
+        static void Main(string[] args)
+        {
+            SerializationEventTestObject obj = new SerializationEventTestObject();
+
+            Console.WriteLine(obj.Member1);
+            Console.WriteLine(obj.Member2);
+            Console.WriteLine(obj.Member3);
+            Console.WriteLine(obj.Member4);
+
+            string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+       
+            Console.WriteLine(obj.Member1);
+            Console.WriteLine(obj.Member2);
+            Console.WriteLine(obj.Member3);
+            Console.WriteLine(obj.Member4);
+
+            obj = JsonConvert.DeserializeObject<SerializationEventTestObject>(json);
+
+            Console.WriteLine(obj.Member1);
+            Console.WriteLine(obj.Member2);
+            Console.WriteLine(obj.Member3);
+            Console.WriteLine(obj.Member4);
+            Console.ReadKey();
+
+        }
+    }
+}
