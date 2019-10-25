023872df9b193481bb3319fba7cb69dcf937b4a5
Thanmai <thanmai.c@bizruntime.net>
2019-10-25 20:19:24 +0530

    Ado.net

---
+++ 25-10-2019/ado_console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ado_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConString = @"Data Source=DESKTOP-M3GIB10;Initial Catalog=Bizruntime;Integrated Security=True";
+            SqlConnection con = new SqlConnection(ConString);
+            string querystring = "Select * from Student";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(querystring, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
