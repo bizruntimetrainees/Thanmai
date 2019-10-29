ef32e3588079ff089d76b332cfc7fdead6af1a68
Thanmai <thanmai.c@bizruntime.net>
2019-10-29 18:55:49 +0530

    web api

---
+++ 29-10-2019/WebApi/Controllers/StudentDetailsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class StudentDetailsController : ApiController
+    {
+     //Get deatils
+        public IHttpActionResult GetAllStudents()
+        {
+            IList<StudentDetailsview> students = null;
+
+            using (var ctx = new BizruntimeEntities1())
+            {
+                students = ctx.StudentDetails.Include("StudentAddress")
+                            .Select(s => new StudentDetailsview()
+                            {
+                                Id = s.Id,
+                                FirstName = s.FirstName,
+                                LastName = s.LastName
+                            }).ToList<StudentDetailsview>();
+            }
+
+            if (students.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(students);
+
+
+        }
+
+        public StudentDetailsController()
+        {
+        }
+
+        //Get action methods of the previous section
+        public IHttpActionResult PostNewStudent(StudentDetailsview student)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BizruntimeEntities1())
+            {
+                ctx.StudentDetails.Add(new StudentDetail()
+                {
+                    StudentId = student.Id.ToString(),
+                    FirstName = student.FirstName,
+                    LastName = student.LastName
+                }) ;
+
+                ctx.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ 29-10-2019/WebApi/Models/StudentDetailsview.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class StudentDetailsview
+    {
+        public string StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Nullable<int> Age { get; set; }
+        public string Gender { get; set; }
+        public string Batch { get; set; }
+        public string Address { get; set; }
+        public string Class { get; set; }
+        public string School { get; set; }
+        public string Domicile { get; set; }
+        public int Id { get; set; }
+    }
+}
