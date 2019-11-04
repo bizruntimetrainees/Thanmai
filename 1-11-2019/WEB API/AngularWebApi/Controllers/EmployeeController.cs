cceb6e55e6191cc880526938bef965fe793da55d
Thanmai <thanmai.c@bizruntime.net>
2019-11-04 14:22:02 +0530

    angular

---
+++ 1-11-2019/WEB API/AngularWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using AngularWebApi.Models;
+
+namespace AngularWebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+            string query = @"select EmployeeID,EmployeeName,Department,MailID, convert(DATETIME,DOJ,120) as DOJ from dbo.Employees";
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        public string Post(Employee emp)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+                string query = @"insert into dbo.Employees 
+                                (
+                                EmployeeName,
+                                Department,
+                                MailID,
+                                DOJ)
+                                Values
+                                (
+                                    ' " + emp.EmployeeName + @" ',
+                                    ' " + emp.Department + @" ' ,
+                                    ' " + emp.MailID + @"'  ,
+                                    ' " +Convert.ToDateTime(emp.DOJ)+ @"'
+                                )";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Added Successfully";
+            }
+            catch(Exception ex)
+            {
+                return "Failed to Add";
+            }
+        }
+    }
+}
---
+++ 31-10-2019/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+export const HEROES:Hero[]=[
+                            { id: 101, name: 'Thanmai' },
+                            { id: 102, name: 'Kiranmai' },
+                            { id: 103, name: 'Jyothirmai' },
+                            { id: 104, name: 'Karan' },
+                            { id: 105, name: 'Chinmai' },
+                            { id: 106, name: 'Sunitha' },
+                            { id: 107, name: 'Diya' },
+                            { id: 108, name: 'Ramani'},
+                            { id: 109, name: 'Megha' },
+                            { id: 110, name: 'Taruni' }
+
+                            ]
+    
